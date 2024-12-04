a0fb32b098c088adc7857ca8e7223c95e62cbcba
Laura <laura.porpiglia@booking.com>
2024-12-04 17:55:59 +0000

    day 2 p2

--- AOC2024/aoc/day2.py
+++ AOC2024/aoc/day2.py
@@ -1,43 +1,46 @@
-# def is_safe(report: list, ignore_index: int) -> bool:
-#     ascending = False
-#     descending = False
-#     previous_level = None
-#
-#     for current_index, current_level in enumerate(report):
-#         if current_index == ignore_index:
-#             continue
-#
-#         if previous_level is None:
-#             previous_level = current_level
-#             continue
-#
-#         if previous_level < current_level:
-#             ascending = True
-#         elif previous_level > current_level:
-#             descending = True
-#
-#         if (previous_level == current_level) or abs(previous_level - current_level) > 3 or (ascending and descending):
-#             return False
-#
-#         previous_level = current_level
-#
-#     return True
-with open("resources/day2input.txt") as f:
-    content = f.read().strip().split('\n')
-
-ans = 0
-for report in content:
-    values = list(map(int, report.split()))
-    safepos = {1, 2, 3}
-    safeneg = {-1, -2, -3}
-    for i in range(1, len(values)):
-        safepos.add(values[i] - values[i - 1])
-        safeneg.add(values[i] - values[i - 1])
-
-    if len(safepos) == 3 or len(safeneg) == 3:
-        ans += 1
-
-print("Day2 p1:",ans)
+def is_safe(report):
+    increasing = all(1 <= report[i + 1] - report[i] <= 3 for i in range(len(report) - 1))
+    decreasing = all(1 <= report[i] - report[i + 1] <= 3 for i in range(len(report) - 1))
+    return increasing or decreasing
 
 
-   
+def is_safe_with_dampener(report):
+    if is_safe(report):
+        return True
+
+    for i in range(len(report)):
+        if is_safe([report[j] for j in range(len(report)) if j != i]):
+            return True
+
+    return False
+
+
+def count_safe_reports(data):
+    safe_count = 0
+    for report in data:
+        if is_safe(report):
+            safe_count += 1
+    return safe_count
+
+
+def count_safe_reports_with_dampener(data):
+    safe_count = 0
+    for report in data:
+        if is_safe_with_dampener(report):
+            safe_count += 1
+    return safe_count
+
+
+def load_data_from_file(filename):
+    with open(filename, 'r') as file:
+        data = [
+            list(map(int, line.strip().split()))
+            for line in file if line.strip()
+        ]
+    return data
+
+
+# if __name__ == "__main__":
+data = load_data_from_file("resources/day2input.txt")
+print("Day2 p1:", count_safe_reports(data))
+print("Day2 p2:", count_safe_reports_with_dampener(data))
